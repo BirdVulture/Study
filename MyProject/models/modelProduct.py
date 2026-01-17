b4441679e8aa350e49dfce42885bdad1e06828af
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2026-01-18 00:28:44 +0700

    model

---
+++ MyProject/models/modelProduct.py
@@ -0,0 +1,23 @@
+# 
+
+
+
+class Product:
+    productName: str
+    productCalory: int
+
+    def __init__(self, productName: str, productCalory: int):
+        self.productName = productName
+        self.productCalory = productCalory
+
+
+
+
+#apple = Product("apple", 14)
+
+#print(apple.productCalory)
+
+
+
+
+
