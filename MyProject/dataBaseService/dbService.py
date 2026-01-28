83cff6b403e3ae6c1927223364e408a36d166277
Oleg Z <157835207+BirdVulture@users.noreply.github.com>
2026-01-28 13:25:46 +0700

    12

--- MyProject/dataBaseService/dbService.py
+++ MyProject/dataBaseService/dbService.py
@@ -2,50 +2,51 @@ import sqlite3
 import queryType as qt
 import queryManger as qm
  
+class DBAction:
 
-
-'''
-class DBmanager:
-    pass
-
-'''        
-
-
-
-
-
-def dataBaseAction(queryType, queryPayload):
-    connection = sqlite3.connect("productsDB")
-
-    dbAction = connection.cursor()
-
-    dbQuery, dbPayload = qm.QueryMaker().makeQuery(queryType, queryPayload)
+    queryResult = None    
     
-  
+    def doQuery(self, queryType, queryPayload):
+        connection = sqlite3.connect("productsDB")
 
-    print(dbQuery)
-    print(dbPayload)
+        dbAction = connection.cursor()
 
-    dbAction.execute(dbQuery, dbPayload) # type: ignore!!!!!
+        dbQuery, dbPayload = qm.QueryMaker().makeQuery(queryType, queryPayload)
+        
     
-    #dbAction.execute('SELECT * FROM products LIMIT ?', (5,)) # Для тестирования SQL запросов
-    
-    queryResult = dbAction.fetchall()
-    
-    print(queryResult)
+
+        print(dbQuery)
+        print(dbPayload)
+
+        #нужно добавить try
+        #нужно добавить обработку результата в зависимости от запроса
+
+        dbAction.execute(dbQuery, dbPayload) # type: ignore
+        
+        #dbAction.execute('SELECT * FROM products LIMIT ?', (5,)) # Для тестирования SQL запросов
+        
+        self.queryResult = dbAction.fetchall()
+        
+        print(self.queryResult)
 
 
 
 
-    connection.commit()
-    connection.close()
+        connection.commit()
+        connection.close()
 
 
 
 # TEST
+testObject = DBAction()
+
+testObject.doQuery(qt.QueryType.READPRODUCTLIST, (10,))
+print(testObject.queryResult)
+
+
 #dataBaseAction(qt.QueryType.CREATEPRODUCT, ("apple", 64))
 
 #dataBaseAction(qt.QueryType.DELETEPRODUCT, (1,))
 
-dataBaseAction(qt.QueryType.READPRODUCTLIST, (5,))
+#dataBaseAction(qt.QueryType.READPRODUCTLIST, (5,))
 
